f84034561356f00891089b8609e43ffbde83c190
Guilherme dezena <guilhermepereiradezena@gmail.com>
2021-04-08 14:26:28 -0300

    Backup

---
+++ cadastrar.php
@@ -0,0 +1,52 @@
+<?php
+require_once 'Classes/usuarios.php';
+$u = new usuario;
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="CSS/estilos.css">
+</head>
+
+<body>
+    <div id="corpo-form-cad">
+        <h1>Cadastrar Usuario</h1>
+        <form method="POST">
+            <input type="text" name="nome" placeholder="Nome Completo" maxlength="30">
+            <input type="text" name="telefone" placeholder="Telefone" maxlength="30">
+            <input type="email" name="email" placeholder="Usuario" maxlength="40">
+            <input type="password" name="senha" placeholder="Senha" maxlength="15">
+            <input type="password" name="confSenha" placeholder="Confirmar senha">
+            <input href="index.php" id="submit" type="submit" value="Cadastrar">
+        </form>
+    </div>
+
+    <?php
+    //verificar se clicou
+    if (isset($_POST['nome'])) {
+        $nome = addslashes($_POST['nome']);
+        $telefone = addslashes($_POST['telefone']);
+        $email = addslashes($_POST['email']);
+        $senha = addslashes($_POST['senha']);
+        $confirmarSenha = addslashes($_POST['confSenha']);
+        if (!empty($nome) && !empty($telefone) && !empty($email) && !empty($senha) && !empty($confirmarSenha)) {
+            $u->conectar("projeto_login", "localhost", "root", "");
+            if ($u->msgErro == "") //se esta tudo okay
+            {
+                $u->cadastrar($nome, $host, $usuario, $senha);
+            } else {
+                echo "Erro:" . $u->msgErro;
+            }
+        } else {
+            echo "Preencha todos os campos!";
+        }
+    }
+
+    ?>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="CSS/estilos.css">
+</head>
+
+<body>
+    <div id="corpo-form">
+        <h1>Cadastre</h1>
+        <form method="POST">
+            <input type="email" name="email" placeholder="Usuario">
+            <input type="password" name="senha" placeholder="Senha">
+            <input  id="submit" type="button" value="Acessar">
+            <a href="cadastrar.php">Ainda não é inscrito?<strong>Cadastre-se aqui</strong></a>
+        </form>
+    </div>
+</body>
+
+</html>
